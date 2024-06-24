a7fc0c587c32e3d3c12203a7e3291c41ad2a6d05
OoshiroHikaru <s2232027bp@s.chibakoudai.jp>
2024-06-24 12:36:34 +0900

    app6.jsを変更

--- app6.js
+++ app6.js
@@ -7,6 +7,9 @@ const db = new sqlite3.Database('test.db');
 app.set('view engine', 'ejs');
 app.use("/public", express.static(__dirname + "/public"));
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
   const message = "Hello world";
   res.render('show', {mes:message});
@@ -38,6 +41,72 @@ app.get("/top", (req, res) => {
         })
     })
 })
+
+app.get("/car_maker", (req, res) => { 
+let sql = ` 
+select car.id, car.name as car_name, maker.name as maker_name 
+from car 
+inner join maker on car.maker_id = maker.id;
+`; 
+
+db.serialize(() => { 
+　 db.all(sql, (error, data) => {
+　　 if (error) { 
+　　　res.render('show', { mes: "エラーです" }); } 
+　　　else {
+ 　　　res.render('select', { data: data});
+　　　 }
+　　 });
+ 　});
+ }); 
+
+app.get("/stpr_man", (req, res) => { 
+let sql = ` 
+select STPR.id, STPR.name as stpr_name, man.name as man_name 
+from STPR 
+inner join man on STPR.stpr_id = man.id; 
+`; 
+
+db.serialize(() => { 
+　db.all(sql, (error, data) => { 
+　　if (error) { 
+　　　res.render('show', { mes: "エラーです" }); 
+　　} else { 
+　　　res.render('select', { data: data }); 
+　　}
+　　});
+　});
+});
+
+app.get("/db/:id", (req, res) => {
+db.serialize( () => {
+db.all("select id, 都道府県, 人口, 大学 from example where id=" + req.params.id + ";", (error, row) => {
+if( error ) {
+res.render('show', {mes:"エラーです"});
+}
+res.render('db', {data:row});
+})
+})
+})
+
+app.post("/insert", (req, res) => {
+ let sql = `
+insert into example (都道府県,人口,大学) values ("` + req.body.name + `",` + req.body.jinko + `,` + req.body.daigaku + `);
+`
+ console.log(sql);
+ db.serialize( () => {
+  db.run( sql, (error, row) => {
+   console.log(error);
+   if(error) {
+    res.render('show', {mes:"エラーです"});
+   }
+   res.redirect('/db');
+  });
+ });
+ console.log(req.body);
+});
+
+
 app.use(function(req, res, next) {
   res.status(404).send('ページが見つかりません');
 });
